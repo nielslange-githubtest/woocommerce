71ece2e9b485165d098c838c88ff7517b6a8560c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-03-18 22:12:24 +0800

    Refactor tests for useSettings hook to improve readability and consistency
    - Update test cases for the useSettings hook to enhance code readability by standardizing formatting and spacing.
    - Ensure consistent use of function calls and assertions throughout the tests.
    - Maintain existing test functionality while improving overall clarity and maintainability.

--- packages/js/data/src/settings/tests/use-settings.test.ts
+++ packages/js/data/src/settings/tests/use-settings.test.ts
@@ -15,73 +15,70 @@ import { createTestSettings } from './utils';
 import { store } from '..';
 
 // Mock data-controls
-jest.mock('@wordpress/data-controls', () => ({
-	...jest.requireActual('@wordpress/data-controls'),
+jest.mock( '@wordpress/data-controls', () => ( {
+	...jest.requireActual( '@wordpress/data-controls' ),
 	apiFetch: jest.fn(),
-}));
+} ) );
 
-jest.mock('../resolvers', () => ({}));
+jest.mock( '../resolvers', () => ( {} ) );
 
 jest.mock( '@wordpress/deprecated', () => jest.fn() );
 
-describe('useSettings', () => {
+describe( 'useSettings', () => {
+	beforeEach( () => {
+		dispatch( store ).clearSettings();
+	} );
 
-	beforeEach(() => {
-		dispatch(store).clearSettings();
-	});
-
-	it('should return settings and utility functions', () => {
+	it( 'should return settings and utility functions', () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['setting1']),
+			() => useSettings( 'general', [ 'setting1' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
 
-		expect(result.current).toEqual({
+		expect( result.current ).toEqual( {
 			settingsError: false,
 			isRequesting: false,
 			isDirty: false,
 			setting1: undefined,
-			updateAndPersistSettings: expect.any(Function),
-			persistSettings: expect.any(Function),
-			updateSettings: expect.any(Function),
-		});
-	});
+			updateAndPersistSettings: expect.any( Function ),
+			persistSettings: expect.any( Function ),
+			updateSettings: expect.any( Function ),
+		} );
+	} );
 
-	it('should update settings', () => {
+	it( 'should update settings', () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['setting1']),
+			() => useSettings( 'general', [ 'setting1' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
 
 		const testSettings = createTestSettings();
 
-		act(() => {
-			result.current.updateSettings(testSettings);
-		});
+		act( () => {
+			result.current.updateSettings( testSettings );
+		} );
 
-		expect(result.current.setting1).toEqual(
-			testSettings.setting1
-		);
-		expect(result.current.isDirty).toBe(true);
-	});
+		expect( result.current.setting1 ).toEqual( testSettings.setting1 );
+		expect( result.current.isDirty ).toBe( true );
+	} );
 
-	it('should persist settings', async () => {
+	it( 'should persist settings', async () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['setting1']),
+			() => useSettings( 'general', [ 'setting1' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
-		(apiFetch as jest.Mock).mockImplementation(
-			({ path, method, data }: APIFetchOptions) => {
-				expect(path).toContain(
+		( apiFetch as jest.Mock ).mockImplementation(
+			( { path, method }: APIFetchOptions ) => {
+				expect( path ).toContain(
 					'/wc-analytics/settings/general/batch'
 				);
-				expect(method).toBe('POST');
+				expect( method ).toBe( 'POST' );
 
 				return Promise.resolve();
 			}
@@ -89,92 +86,91 @@ describe('useSettings', () => {
 
 		const testSettings = createTestSettings();
 
-		act(() => {
-			result.current.updateSettings(testSettings);
-		});
+		act( () => {
+			result.current.updateSettings( testSettings );
+		} );
 
-		await act(async () => {
+		await act( async () => {
 			await result.current.persistSettings();
-		});
+		} );
 
-		expect(result.current.setting1).toEqual(
-			testSettings.setting1
-		);
-		expect(result.current.isDirty).toBe(false);
-	});
+		expect( result.current.setting1 ).toEqual( testSettings.setting1 );
+		expect( result.current.isDirty ).toBe( false );
+	} );
 
-	it('should update and persist settings', async () => {
+	it( 'should update and persist settings', async () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['setting1']),
+			() => useSettings( 'general', [ 'setting1' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
 
-		(apiFetch as jest.Mock).mockImplementation(
-			({ path, method, data }: APIFetchOptions) => {
-				expect(path).toContain(
+		( apiFetch as jest.Mock ).mockImplementation(
+			( { path, method }: APIFetchOptions ) => {
+				expect( path ).toContain(
 					'/wc-analytics/settings/general/batch'
 				);
-				expect(method).toBe('POST');
+				expect( method ).toBe( 'POST' );
 
 				return Promise.resolve();
 			}
 		);
-
 
 		const testSettings = createTestSettings();
 
-		await act(async () => {
-			await result.current.updateAndPersistSettings(testSettings);
-		});
+		await act( async () => {
+			await result.current.updateAndPersistSettings( testSettings );
+		} );
 
-		expect(result.current.setting1).toEqual(testSettings.setting1);
-		expect(result.current.isDirty).toBe(false);
-	});
+		expect( result.current.setting1 ).toEqual( testSettings.setting1 );
+		expect( result.current.isDirty ).toBe( false );
+	} );
 
-	it('should update settings with name and settings object', () => {
+	it( 'should update settings with name and settings object', () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['newSetting']),
+			() => useSettings( 'general', [ 'newSetting' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
 
-		act(() => {
-			result.current.updateSettings('general', { newSetting: 'new' });
-		});
+		act( () => {
+			result.current.updateSettings( 'general', { newSetting: 'new' } );
+		} );
 
-		expect(result.current.newSetting).toEqual( 'new' );
-		expect(result.current.isDirty).toBe(true);
-		expect(deprecated).toHaveBeenCalled();
-	});
+		expect( result.current.newSetting ).toEqual( 'new' );
+		expect( result.current.isDirty ).toBe( true );
+		expect( deprecated ).toHaveBeenCalled();
+	} );
 
-	it('should update and persist settings with name and settings object', async () => {
+	it( 'should update and persist settings with name and settings object', async () => {
 		const { result } = renderHook(
-			() => useSettings('general', ['newSetting']),
+			() => useSettings( 'general', [ 'newSetting' ] ),
 			{
-				wrapper: ({ children }) => children,
+				wrapper: ( { children } ) => children,
 			}
 		);
 
-		(apiFetch as jest.Mock).mockImplementation(
-			({ path, method, data }: APIFetchOptions) => {
-				expect(path).toContain(
+		( apiFetch as jest.Mock ).mockImplementation(
+			( { path, method }: APIFetchOptions ) => {
+				expect( path ).toContain(
 					'/wc-analytics/settings/general/batch'
 				);
-				expect(method).toBe('POST');
+				expect( method ).toBe( 'POST' );
 
 				return Promise.resolve();
 			}
 		);
 
-		await act(async () => {
-			await result.current.updateAndPersistSettings('general', { newSetting: 'new' });
-		});
+		await act( async () => {
+			await result.current.updateAndPersistSettings( 'general', {
+				newSetting: 'new',
+			} );
+		} );
 
-		expect(result.current.newSetting).toEqual('new');
-		expect(result.current.isDirty).toBe(false);
-		expect(deprecated).toHaveBeenCalled();
-	});
-});
+		expect( result.current.newSetting ).toEqual( 'new' );
+		expect( result.current.isDirty ).toBe( false );
+		expect( deprecated ).toHaveBeenCalled();
+	} );
+} );
