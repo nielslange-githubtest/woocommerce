540f31593420fd6f15626c40f8e5bf3ad0630fa3
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-03-18 18:46:27 +0800

    Enhance useSettings hook with improved type definitions and documentation
    - Refactor the useSettings hook to provide clearer type definitions for its parameters and return value.
    - Update the implementation to support both string and object formats for settings updates, enhancing backward compatibility.
    - Improve documentation comments to clarify the purpose and usage of the hook, making it easier for developers to understand its functionality.

--- packages/js/data/src/settings/use-settings.ts
+++ packages/js/data/src/settings/use-settings.ts
@@ -10,7 +10,30 @@ import { useCallback } from '@wordpress/element';
 import { store } from './';
 import type { Settings } from './types';
 
-export const useSettings = ( group: string, settingsKeys: string[] = [] ) => {
+/**
+ * Custom hook to manage settings for a given group.
+ *
+ * @param {string}   group          - The group of settings to manage.
+ * @param {string[]} [settingsKeys] - The keys of the settings to manage.
+ * @return {Object} An object containing the settings, error, requesting status, and dirty status.
+ */
+export const useSettings: (
+	group: string,
+	settingsKeys?: string[]
+) => {
+	persistSettings: () => Promise< void >;
+	updateAndPersistSettings: {
+		( name: string, settings: Settings ): Promise< void >;
+		( settings: Settings ): Promise< void >;
+	};
+	updateSettings: {
+		( name: string, settings: Settings ): void;
+		( settings: Settings ): void;
+	};
+	settingsError: boolean;
+	isRequesting: boolean | undefined;
+	isDirty: boolean;
+} & Settings = ( group: string, settingsKeys: string[] = [] ) => {
 	const { requestedSettings, settingsError, isRequesting, isDirty } =
 		useSelect(
 			( select ) => {
@@ -20,6 +43,7 @@ export const useSettings = ( group: string, settingsKeys: string[] = [] ) => {
 					getIsDirty,
 					isUpdateSettingsRequesting,
 				} = select( store );
+
 				return {
 					requestedSettings: getSettingsForGroup(
 						group,
@@ -32,30 +56,53 @@ export const useSettings = ( group: string, settingsKeys: string[] = [] ) => {
 					isDirty: getIsDirty( group, settingsKeys ),
 				};
 			},
+			// Ignore lint error because we sort the settingsKeys array
+			// eslint-disable-next-line react-hooks/exhaustive-deps
 			[ group, ...settingsKeys.sort() ]
 		);
+
 	const {
 		persistSettingsForGroup,
 		updateAndPersistSettingsForGroup,
 		updateSettingsForGroup,
 	} = useDispatch( store );
+
 	const updateSettings = useCallback(
-		( name: string, data: Settings ) => {
-			updateSettingsForGroup( group, { [ name ]: data } );
+		( nameOrSettings: string | Settings, maybeSettings?: Settings ) => {
+			if ( typeof nameOrSettings === 'string' && maybeSettings ) {
+				updateSettingsForGroup( group, {
+					[ nameOrSettings ]: maybeSettings,
+				} );
+			} else if ( typeof nameOrSettings === 'object' ) {
+				updateSettingsForGroup( group, nameOrSettings );
+			}
 		},
-		[ group ]
+		[ group, updateSettingsForGroup ]
 	);
-	const persistSettings = useCallback( () => {
+
+	const persistSettings = useCallback( async () => {
 		// this action would simply persist all settings marked as dirty in the
 		// store state and then remove the dirty record in the isDirtyMap
-		persistSettingsForGroup( group );
-	}, [ group ] );
+		await persistSettingsForGroup( group );
+	}, [ group, persistSettingsForGroup ] );
+
 	const updateAndPersistSettings = useCallback(
-		( name: string, data: Settings ) => {
-			updateAndPersistSettingsForGroup( group, { [ name ]: data } );
+		async (
+			nameOrSettings: string | Settings,
+			maybeSettings?: Settings
+		) => {
+			// Backwards compatibility for the old interface
+			if ( typeof nameOrSettings === 'string' && maybeSettings ) {
+				await updateAndPersistSettingsForGroup( group, {
+					[ nameOrSettings ]: maybeSettings,
+				} );
+			} else if ( typeof nameOrSettings === 'object' ) {
+				await updateAndPersistSettingsForGroup( group, nameOrSettings );
+			}
 		},
-		[ group ]
+		[ group, updateAndPersistSettingsForGroup ]
 	);
+
 	return {
 		settingsError,
 		isRequesting,
