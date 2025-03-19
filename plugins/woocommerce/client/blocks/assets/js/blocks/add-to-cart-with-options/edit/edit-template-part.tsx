bcb1d5384b9d2014153ad7f1009f9a131e2993be
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-03-19 15:25:13 +0100

    [WIP] [Experimental] Make the Add to Cart with Options template part not editable for users without permissions

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/edit/edit-template-part.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/edit/edit-template-part.tsx
@@ -1,14 +1,100 @@
 /**
  * External dependencies
  */
-import { useSelect } from '@wordpress/data';
+import { useEffect, useMemo, useState } from '@wordpress/element';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { Spinner } from '@wordpress/components';
 import { useEntityBlockEditor, store as coreStore } from '@wordpress/core-data';
 import {
 	InnerBlocks,
 	useInnerBlocksProps,
 	useBlockProps,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
+import { parse } from '@wordpress/blocks';
+
+const NonEditableTemplatePartPreview = ( {
+	blockProps,
+	templatePartId,
+}: {
+	blockProps: Record< string, unknown >;
+	templatePartId: string | undefined;
+} ) => {
+	const [ isBlockEditingModeDisabled, setIsBlockEditingModeDisabled ] =
+		useState( false );
+
+	const { content, editedBlocks } = useSelect(
+		( select ) => {
+			if ( ! templatePartId ) {
+				return {};
+			}
+			const { getEditedEntityRecord } = select( coreStore );
+			const editedRecord = getEditedEntityRecord(
+				'postType',
+				'wp_template_part',
+				templatePartId
+			);
+			return {
+				editedBlocks: editedRecord?.blocks,
+				content: editedRecord?.content,
+			};
+		},
+		[ templatePartId ]
+	);
+
+	const blocks = useMemo( () => {
+		if ( ! templatePartId ) {
+			return undefined;
+		}
+
+		if ( editedBlocks ) {
+			return editedBlocks;
+		}
+
+		if ( ! content || typeof content !== 'string' ) {
+			return [];
+		}
+
+		return parse( content );
+	}, [ templatePartId, editedBlocks, content ] );
+
+	const { setBlockEditingMode, unsetBlockEditingMode } =
+		useDispatch( blockEditorStore );
+
+	useEffect( () => {
+		blocks.forEach( ( block ) => {
+			setBlockEditingMode( block.clientId, 'disabled' );
+		} );
+		setIsBlockEditingModeDisabled( true );
+		return () => {
+			blocks.forEach( ( block ) => {
+				unsetBlockEditingMode( block.clientId );
+			} );
+			setIsBlockEditingModeDisabled( false );
+		};
+	}, [ setBlockEditingMode, unsetBlockEditingMode, blocks ] );
+
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		value: blocks,
+		onInput: () => {},
+		onChange: () => {},
+		renderAppender: null,
+	} );
+
+	if (
+		! isBlockEditingModeDisabled ||
+		! Array.isArray( blocks ) ||
+		blocks.length === 0
+	) {
+		return (
+			<div { ...blockProps }>
+				<Spinner />
+			</div>
+		);
+	}
+
+	return <div { ...innerBlocksProps } />;
+};
 
 const TemplatePartInnerBlocks = ( {
 	blockProps,
@@ -119,6 +205,27 @@ export const AddToCartWithOptionsEditTemplatePart = ( {
 
 	const blockProps = useBlockProps();
 
+	const { canViewTemplatePart, canEditTemplatePart } = useSelect(
+		( select ) => {
+			const templatePartData = {
+				kind: 'postType',
+				name: 'wp_template_part',
+				id: templatePartId,
+			};
+			return {
+				canViewTemplatePart: !! select( coreStore ).canUser(
+					'read',
+					templatePartData
+				),
+				canEditTemplatePart: !! select( coreStore ).canUser(
+					'update',
+					templatePartData
+				),
+			};
+		},
+		[ templatePartId ]
+	);
+
 	if ( ! templatePartId ) {
 		return (
 			<div { ...blockProps }>
@@ -127,8 +234,17 @@ export const AddToCartWithOptionsEditTemplatePart = ( {
 		);
 	}
 
-	return (
+	if ( ! canViewTemplatePart ) {
+		return null;
+	}
+
+	return canEditTemplatePart ? (
 		<TemplatePartInnerBlocks
+			blockProps={ blockProps }
+			templatePartId={ templatePartId }
+		/>
+	) : (
+		<NonEditableTemplatePartPreview
 			blockProps={ blockProps }
 			templatePartId={ templatePartId }
 		/>
