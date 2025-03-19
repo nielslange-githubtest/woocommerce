e096c5f64964b41bb03a3aead8e3e0d68586665f
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-19 15:02:22 +0530

    use withQueryLoopProductContextValidation HOC in Product Reviews block
    The Product Reviews block now uses the withQueryLoopProductContextValidation HOC
    to handle validation of the block's context within Query Loops. This replaces
    the custom useIsInvalidQueryLoopContext hook with shared functionality.

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
@@ -3,14 +3,11 @@
  */
 
 import { SelectControl } from '@wordpress/components';
-import { useSelect } from '@wordpress/data';
-import { useMemo } from '@wordpress/element';
+import { withQueryLoopProductContextValidation } from '@woocommerce/block-hocs';
 import { __ } from '@wordpress/i18n';
 import {
 	InspectorControls,
 	useBlockProps,
-	Warning,
-	store as blockEditorStore,
 	// @ts-expect-error missing types.
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
@@ -22,51 +19,12 @@ import TEMPLATE from './template';
 import { ProductReviewsEditProps } from './types';
 import { htmlElementMessages } from '../../utils/messages';
 
-/**
- * Check if block is inside a Query Loop with non-product post type
- *
- * @param {string} clientId The block's client ID
- * @param {string} postType The current post type
- * @return {boolean} Whether the block is in an invalid Query Loop context
- */
-const useIsInvalidQueryLoopContext = ( clientId: string, postType: string ) => {
-	const { getBlockParentsByBlockName } = useSelect( blockEditorStore, [] );
-	const blockParents = useMemo( () => {
-		return getBlockParentsByBlockName( clientId, 'core/post-template' );
-	}, [ getBlockParentsByBlockName, clientId ] );
-
-	return blockParents.length > 0 && postType !== 'product';
-};
-
-const Edit = ( {
-	attributes,
-	setAttributes,
-	clientId,
-	context,
-}: ProductReviewsEditProps ) => {
+const Edit = ( { attributes, setAttributes }: ProductReviewsEditProps ) => {
 	const { tagName: TagName } = attributes;
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		template: TEMPLATE,
 	} );
-
-	const { postType: contextPostType } = context;
-	const isInvalidQueryLoopContext = useIsInvalidQueryLoopContext(
-		clientId,
-		contextPostType
-	);
-	if ( isInvalidQueryLoopContext ) {
-		return (
-			<div { ...blockProps }>
-				<Warning>
-					{ __(
-						'The Product Reviews block requires a product context. When used in a Query Loop, the Query Loop must be configured to display products.',
-						'woocommerce'
-					) }
-				</Warning>
-			</div>
-		);
-	}
 
 	return (
 		<>
@@ -97,4 +55,4 @@ const Edit = ( {
 	);
 };
 
-export default Edit;
+export default withQueryLoopProductContextValidation( Edit, 'Product Reviews' );
