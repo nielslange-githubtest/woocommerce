27391bfb0b62acb0031251f65c40f28557242923
Vladimir Reznichenko <kalessil@gmail.com>
2025-03-20 11:40:19 +0100

    Performance: queue implementations.

--- plugins/woocommerce/src/Internal/Queue/DefaultQueueFifo.php
+++ plugins/woocommerce/src/Internal/Queue/DefaultQueueFifo.php
@@ -4,5 +4,9 @@ declare( strict_types = 1);
 
 namespace Automattic\WooCommerce\Internal\Queue;
 
+/**
+ * This class represents `\WC_Action_Queue` from legacy components namespace.
+ * It mainly serves to provide clarity on the domain structure in this namespace.
+ */
 final class DefaultQueueFifo extends \WC_Action_Queue implements QueueFifoInterface {
 }
