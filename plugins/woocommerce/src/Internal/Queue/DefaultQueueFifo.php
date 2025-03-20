d60a8a3fee6b4a539a80875ed959799c4fec3802
Vladimir Reznichenko <kalessil@gmail.com>
2025-03-20 10:44:06 +0100

    Performance: compatibility - FIFO entries.

---
+++ plugins/woocommerce/src/Internal/Queue/DefaultQueueFifo.php
@@ -0,0 +1,8 @@
+<?php
+
+declare( strict_types = 1);
+
+namespace Automattic\WooCommerce\Internal\Queue;
+
+final class DefaultQueueFifo extends \WC_Action_Queue implements QueueFifoInterface {
+}
---
+++ plugins/woocommerce/src/Internal/Queue/QueueFifoInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+declare( strict_types = 1);
+
+namespace Automattic\WooCommerce\Internal\Queue;
+
+interface QueueFifoInterface extends \WC_Queue_Interface {
+}
