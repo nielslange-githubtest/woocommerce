c08253639c1e9f686cd963f273b4249e28192448
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-23 17:32:48 +0530

    Add styles for product attributes block
    I copied legacy styles for now

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductAttributes.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductAttributes.php
@@ -14,11 +14,11 @@ class ProductAttributes extends AbstractBlock {
 	protected $block_name = 'product-attributes';
 
 	/**
-	 * Get the frontend style handle for this block type.
+	 * Get the frontend script handle for this block type.
 	 *
-	 * @return string[]|null
+	 * @param string $key Data to get, or default to everything.
 	 */
-	protected function get_block_type_style() {
+	protected function get_block_type_script( $key = null ) {
 		return null;
 	}
 
