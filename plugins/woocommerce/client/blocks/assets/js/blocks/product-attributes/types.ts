43b1be67f47fafd6f91cdfbf5f06236d60fa0dfb
Manish Menaria <the.manish.menaria@gmail.com>
2025-03-23 17:56:54 +0530

    Remove unused tagName attribute

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-attributes/types.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-attributes/types.ts
@@ -3,12 +3,8 @@
  */
 import { BlockEditProps } from '@wordpress/blocks';
 
-type Attributes = {
-	tagName: 'div' | 'section' | 'aside';
-};
-
 type Context = {
 	context: { postId: string; postType: string };
 };
 
-export type ProductAttributesEditProps = BlockEditProps< Attributes > & Context;
+export type ProductAttributesEditProps = BlockEditProps< object > & Context;
