041cfefc0247b8e61bbb895ced426efd801bf741
Maikel Perez <mdperez86@gmail.com>
2025-03-26 10:25:19 -0400

    Fix block style and script resolution

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
@@ -282,4 +282,13 @@ class AddToCartWithOptions extends AbstractBlock {
 	protected function get_block_type_script( $key = null ) {
 		return null;
 	}
+
+	/**
+	 * Get the frontend style handle for this block type.
+	 *
+	 * @return null
+	 */
+	protected function get_block_type_style() {
+		return null;
+	}
 }
--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
@@ -18,6 +18,16 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 	protected $block_name = 'add-to-cart-with-options-variation-selector-attribute-options';
 
 	/**
+	 * Disable the frontend script for this block type, it's built with script modules.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 * @return array|string|null
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
+
+	/**
 	 * Get the block's attributes.
 	 *
 	 * @param array $attributes Block attributes. Default empty array.
