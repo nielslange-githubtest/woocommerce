f52568f9675a147aa5d6318297266b6c25cfe844
Ahmed <ahmed.el.azzabi@automattic.com>
2025-03-26 14:06:06 +0000

    Remove unnecessary data

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
@@ -4,7 +4,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 
 import apiFetch from '@wordpress/api-fetch';
-import { addQueryArgs } from '@wordpress/url';
 import { Loader } from '@woocommerce/onboarding';
 import { __ } from '@wordpress/i18n';
 
@@ -15,9 +14,6 @@ import WooPaymentsStepHeader from '../../components/header';
 import { useOnboardingContext } from '../../data/onboarding-context';
 import { WC_ASSET_URL } from '~/utils/admin-settings';
 import './style.scss';
-interface AccountData {
-	status: string;
-}
 
 interface StepCheckResponse {
 	status: string;
@@ -51,19 +47,10 @@ const TestDriveLoader: React.FunctionComponent< {
 	</Loader>
 );
 
-const TestAccountStep = ( {
-	connectUrl,
-	checkUrl,
-}: {
-	connectUrl: string;
-	checkUrl: string;
-} ) => {
-	const { currentStep, navigateToNextStep, refreshOnboardingSteps } =
-		useOnboardingContext();
+const TestAccountStep = () => {
+	const { currentStep, refreshOnboardingSteps } = useOnboardingContext();
 	const [ testDriveLoaderProgress, setTestDriveLoaderProgress ] =
 		useState( 5 );
-
-	const [ errorMessage, setErrorMessage ] = useState( '' );
 
 	// Create a reference object.
 	const loaderProgressRef = useRef( testDriveLoaderProgress );
@@ -114,82 +101,6 @@ const TestAccountStep = ( {
 		refreshOnboardingSteps,
 	] );
 
-	// Use a timer to track the elapsed time for the test drive mode setup.
-	let testDriveSetupStartTime: number;
-	// The test drive setup will be forced finished after 40 seconds
-	// (10 seconds for the initial calls plus 30 for checking the account status in a loop).
-	const testDriveSetupMaxDuration = 40;
-
-	// Helper function to calculate the elapsed time in seconds.
-	const elapsed = ( time: number ) =>
-		Math.round( ( Date.now() - time ) / 1000 );
-
-	const urlParams = new URLSearchParams( window.location.search );
-
-	const checkAccountStatus = ( extraQueryArgs = {} ) => {
-		// Fetch account status from the cache.
-		apiFetch( {
-			path: checkUrl,
-			method: 'GET',
-		} ).then( ( account ) => {
-			// Simulate the update of the loader progress bar by 4% per check.
-			// Limit to a maximum of 10 checks (6% progress per each request starting from 40% = max 10 checks).
-			updateLoaderProgress( 100, 6 );
-
-			// If the account status is not a pending one, the progress percentage is above 95,
-			// or we've exceeded the timeout, consider our work done and redirect the merchant.
-			// Otherwise, schedule another check after a 2.5 seconds wait.
-			if (
-				( account &&
-					( account as AccountData ).status &&
-					! ( account as AccountData ).status.includes(
-						'pending'
-					) ) ||
-				loaderProgressRef.current > 95 ||
-				elapsed( testDriveSetupStartTime ) > testDriveSetupMaxDuration
-			) {
-				setTestDriveLoaderProgress( 100 );
-				const queryArgs = {
-					test_drive: 'true',
-					'wcpay-sandbox-success': 'true',
-					source: '',
-					from: 'WCPAY_CONNECT',
-					redirect_to_settings_page:
-						urlParams.get( 'redirect_to_settings_page' ) || '',
-				};
-
-				// Redirect to the Connect URL and let it figure it out where to point the merchant.
-				window.location.href = addQueryArgs( connectUrl, {
-					...queryArgs,
-					...extraQueryArgs,
-				} );
-			} else {
-				// Schedule another check after 2.5 seconds.
-				// 2.5 seconds plus 0.5 seconds for the fetch request is 3 seconds.
-				// With a maximum of 10 checks, we will wait for 30 seconds before ending the process normally.
-				setTimeout( () => checkAccountStatus( extraQueryArgs ), 2500 );
-			}
-		} );
-	};
-
-	let isAccountSetupSessionError = false;
-	// Determine if we have the account session error message since we want to customize the UX a little bit.
-	if ( errorMessage && errorMessage.includes( 'account setup session' ) ) {
-		isAccountSetupSessionError = true;
-	}
-
-	const isAccountTestDriveError =
-		urlParams.get( 'test_drive_error' ) === 'true';
-	if ( ! errorMessage && isAccountTestDriveError ) {
-		// If there isn't an error message from elsewhere, but we have a test drive error,
-		// show the test drive error message.
-		setErrorMessage(
-			__(
-				'An error occurred while setting up your sandbox account. Please try again!',
-				'woocommerce'
-			)
-		);
-	}
 	return (
 		<div className="woocommerce-payments-test-account-step">
 			<WooPaymentsStepHeader onClose={ () => {} } />
