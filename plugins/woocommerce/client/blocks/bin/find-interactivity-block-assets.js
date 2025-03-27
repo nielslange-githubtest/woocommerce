ac8876c15957f2193e066d55f4d1c2bb68cf5d67
Sam Seay <samueljseay@gmail.com>
2025-03-27 19:57:00 +0800

    Find entry points by convention and add them to the entries list in webpack config.

---
+++ plugins/woocommerce/client/blocks/bin/find-interactivity-block-assets.js
@@ -0,0 +1,38 @@
+const path = require( 'path' );
+const fs = require( 'fs' );
+const glob = require( 'glob' );
+
+function findInteractivityBlockAssets( dir, additionalPatterns = [] ) {
+	let results = [];
+	const ents = fs.readdirSync( dir, { withFileTypes: true } );
+
+	for ( const entry of ents ) {
+		const fullPath = path.join( dir, entry.name );
+		if ( entry.isDirectory() ) {
+			results = results.concat(
+				findInteractivityBlockAssets( fullPath, additionalPatterns )
+			);
+		} else if ( entry.isFile() && entry.name === 'block.json' ) {
+			// parse the json file and determine if its a block that supports interactivity.
+			const blockJson = JSON.parse( fs.readFileSync( fullPath, 'utf8' ) );
+
+			if ( blockJson.supports && blockJson.supports.interactivity ) {
+				const blockDir = path.dirname( fullPath );
+				const adjacentFiles = additionalPatterns.flatMap( ( pattern ) =>
+					glob.sync( pattern, { cwd: blockDir, absolute: true } )
+				);
+				results.push( {
+					blockName: blockJson.name,
+					blockJson: fullPath,
+					adjacentFiles,
+				} );
+			}
+		}
+	}
+
+	return results;
+}
+
+module.exports = {
+	findInteractivityBlockAssets,
+};
--- plugins/woocommerce/client/blocks/bin/webpack-config-interactivity-blocks-frontend.js
+++ plugins/woocommerce/client/blocks/bin/webpack-config-interactivity-blocks-frontend.js
@@ -3,48 +3,81 @@
  */
 const path = require( 'path' );
 const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const [
+	,
+	moduleConfig,
+] = require( '@wordpress/scripts/config/webpack.config' );
+const RemoveFilesPlugin = require( './remove-files-webpack-plugin' );
 
 /**
  * Internal dependencies
  */
 const { sharedOptimizationConfig } = require( './webpack-shared-config' );
+const {
+	findInteractivityBlockAssets,
+} = require( './find-interactivity-block-assets' );
+
+const interactivityBlocks = findInteractivityBlockAssets(
+	path.resolve( __dirname, '../assets/js' ),
+	[ 'frontend.*s', 'style.scss' ]
+);
+
+const scriptModuleEntries = interactivityBlocks.map( ( b ) => {
+	return {
+		[ b.blockName ]: b.adjacentFiles.find( ( f ) =>
+			f.includes( 'frontend' )
+		),
+	};
+} );
+
+const styleEntries = interactivityBlocks.map( ( b ) => {
+	return {
+		[ `${ b.blockName }-style` ]: b.adjacentFiles.find( ( f ) =>
+			f.includes( 'style' )
+		),
+	};
+} );
 
 const entries = {
 	// Blocks
-	'woocommerce/product-button':
-		'./assets/js/atomic/blocks/product-elements/button/frontend.ts',
-	'woocommerce/product-gallery':
-		'./assets/js/blocks/product-gallery/frontend.ts',
-	'woocommerce/product-gallery-large-image':
-		'./assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/frontend.ts',
-	'woocommerce/product-collection':
-		'./assets/js/blocks/product-collection/frontend.ts',
-	'woocommerce/product-filters':
-		'./assets/js/blocks/product-filters/frontend.ts',
-	'woocommerce/product-filter-active':
-		'./assets/js/blocks/product-filters/inner-blocks/active-filters/frontend.ts',
-	'woocommerce/product-filter-checkbox-list':
-		'./assets/js/blocks/product-filters/inner-blocks/checkbox-list/frontend.ts',
-	'woocommerce/product-filter-chips':
-		'./assets/js/blocks/product-filters/inner-blocks/chips/frontend.ts',
-	'woocommerce/product-filter-price':
-		'./assets/js/blocks/product-filters/inner-blocks/price-filter/frontend.ts',
-	'woocommerce/product-filter-price-slider':
-		'./assets/js/blocks/product-filters/inner-blocks/price-slider/frontend.ts',
-	'woocommerce/accordion-group':
-		'./assets/js/blocks/accordion/accordion-group/frontend.js',
-	'woocommerce/add-to-cart-form':
-		'./assets/js/blocks/product-elements/add-to-cart-form/frontend.ts',
-	'woocommerce/add-to-cart-with-options':
-		'./assets/js/blocks/add-to-cart-with-options/frontend.ts',
-	'woocommerce/add-to-cart-with-options-grouped-product-selector':
-		'./assets/js/blocks/add-to-cart-with-options/grouped-product-selector/frontend.ts',
-	'woocommerce/add-to-cart-with-options-quantity-selector':
-		'./assets/js/blocks/add-to-cart-with-options/quantity-selector/frontend.ts',
-	'woocommerce/add-to-cart-with-options-variation-selector':
-		'./assets/js/blocks/add-to-cart-with-options/variation-selector/frontend.ts',
-	'woocommerce/add-to-cart-with-options-variation-selector-attribute-options':
-		'./assets/js/blocks/add-to-cart-with-options/variation-selector/attribute-options/frontend.ts',
+	// 'woocommerce/product-button':
+	// 	'./assets/js/atomic/blocks/product-elements/button/frontend.ts',
+	// 'woocommerce/product-gallery':
+	// 	'./assets/js/blocks/product-gallery/frontend.ts',
+	// 'woocommerce/product-gallery-large-image':
+	// 	'./assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/frontend.ts',
+	// 'woocommerce/product-collection':
+	// 	'./assets/js/blocks/product-collection/frontend.ts',
+	// 'woocommerce/product-filters':
+	// 	'./assets/js/blocks/product-filters/frontend.ts',
+	// 'woocommerce/product-filter-active':
+	// 	'./assets/js/blocks/product-filters/inner-blocks/active-filters/frontend.ts',
+	// 'woocommerce/product-filter-checkbox-list':
+	// 	'./assets/js/blocks/product-filters/inner-blocks/checkbox-list/frontend.ts',
+	// 'woocommerce/product-filter-chips':
+	// 	'./assets/js/blocks/product-filters/inner-blocks/chips/frontend.ts',
+	// 'woocommerce/product-filter-price':
+	// 	'./assets/js/blocks/product-filters/inner-blocks/price-filter/frontend.ts',
+	// 'woocommerce/product-filter-price-slider':
+	// 	'./assets/js/blocks/product-filters/inner-blocks/price-slider/frontend.ts',
+	// 'woocommerce/accordion-group':
+	// 	'./assets/js/blocks/accordion/accordion-group/frontend.js',
+	// 'woocommerce/add-to-cart-form':
+	// 	'./assets/js/blocks/product-elements/add-to-cart-form/frontend.ts',
+	// 'woocommerce/add-to-cart-with-options':
+	// 	'./assets/js/blocks/add-to-cart-with-options/frontend.ts',
+	// 'woocommerce/add-to-cart-with-options-grouped-product-selector':
+	// 	'./assets/js/blocks/add-to-cart-with-options/grouped-product-selector/frontend.ts',
+	// 'woocommerce/add-to-cart-with-options-quantity-selector':
+	// 	'./assets/js/blocks/add-to-cart-with-options/quantity-selector/frontend.ts',
+	// 'woocommerce/add-to-cart-with-options-variation-selector':
+	// 	'./assets/js/blocks/add-to-cart-with-options/variation-selector/frontend.ts',
+	// 'woocommerce/add-to-cart-with-options-variation-selector-attribute-options':
+	// 	'./assets/js/blocks/add-to-cart-with-options/variation-selector/attribute-options/frontend.ts',
+
+	...scriptModuleEntries,
+	...styleEntries,
 
 	// Other
 	'@woocommerce/stores/woocommerce/cart':
@@ -76,6 +109,9 @@ module.exports = {
 		extensions: [ '.js', '.ts', '.tsx' ],
 	},
 	plugins: [
+		new MiniCssExtractPlugin( {
+			filename: '[name].css',
+		} ),
 		new DependencyExtractionWebpackPlugin( {
 			combineAssets: true,
 			combinedOutputFile: './interactivity-blocks-frontend-assets.php',
@@ -85,41 +121,63 @@ module.exports = {
 				}
 			},
 		} ),
+		// Remove JS files generated by MiniCssExtractPlugin.
+		new RemoveFilesPlugin( './build/**/*style.js' ),
 	],
 	module: {
 		rules: [
+			...moduleConfig.module.rules.filter(
+				( rule ) =>
+					! rule.test.test( '.css' ) &&
+					! rule.test.test( '.scss' ) &&
+					! rule.test.test( '.sass' )
+			),
 			{
-				test: /\.(j|t)sx?$/,
-				exclude: [ /[\/\\](node_modules|build|docs|vendor)[\/\\]/ ],
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							[
-								'@wordpress/babel-preset-default',
-								{
-									modules: false,
-									targets: {
-										browsers: [
-											'extends @wordpress/browserslist-config',
-										],
-									},
-								},
-							],
-						],
-						cacheDirectory: path.resolve(
-							__dirname,
-							'../../../node_modules/.cache/babel-loader'
-						),
-						cacheCompression: false,
+				test: /\.s?css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							sassOptions: {
+								includePaths: [ 'assets/css/abstracts' ],
+							},
+							additionalData: ( content, loaderContext ) => {
+								const { resourcePath, rootContext } =
+									loaderContext;
+								const relativePath = path.relative(
+									rootContext,
+									resourcePath
+								);
+
+								if (
+									relativePath.startsWith(
+										'assets/css/abstracts/'
+									) ||
+									relativePath.startsWith(
+										'assets\\css\\abstracts\\'
+									)
+								) {
+									return content;
+								}
+
+								return (
+									'@use "sass:math";' +
+									'@use "sass:string";' +
+									'@use "sass:color";' +
+									'@use "sass:map";' +
+									'@import "_colors"; ' +
+									'@import "_variables"; ' +
+									'@import "_breakpoints"; ' +
+									'@import "_mixins"; ' +
+									content
+								);
+							},
+						},
 					},
-				},
-			},
-			{
-				test: /\.s[c|a]ss$/,
-				use: {
-					loader: 'ignore-loader',
-				},
+				],
 			},
 		],
 	},
