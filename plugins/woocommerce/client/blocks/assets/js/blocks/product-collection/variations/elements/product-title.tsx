e4f72e164bd47da563729e45298024e62420fe2d
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-04-01 10:06:02 +0200

    Fix lint

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/variations/elements/product-title.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/variations/elements/product-title.tsx
@@ -3,15 +3,15 @@
  */
 import { Icon } from '@wordpress/components';
 import { heading } from '@wordpress/icons';
+import {
+	title,
+	description,
+} from '@woocommerce/atomic-blocks/product-elements/title/block.json';
 
 /**
  * Internal dependencies
  */
 import { registerElementVariation } from './utils';
-import {
-	title,
-	description,
-} from '@woocommerce/atomic-blocks/product-elements/title/block.json';
 import blockJson from '../../block.json';
 
 export const CORE_NAME = 'core/post-title';
