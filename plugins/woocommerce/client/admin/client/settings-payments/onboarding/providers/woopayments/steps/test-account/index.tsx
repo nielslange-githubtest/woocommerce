dd1a209c0428cc552cf1afe650b5fa29d06f81e1
Ahmed <ahmed.el.azzabi@automattic.com>
2025-04-02 14:49:51 +0000

    Add support for finish screen

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
@@ -5,7 +5,10 @@ import React, { useState, useRef, useEffect } from 'react';
 import apiFetch from '@wordpress/api-fetch';
 import { Loader } from '@woocommerce/onboarding';
 import { __ } from '@wordpress/i18n';
-import { Notice } from '@wordpress/components';
+import interpolateComponents from '@automattic/interpolate-components';
+import { Notice, Button } from '@wordpress/components';
+import { Link } from '@woocommerce/components';
+import { navigateTo, getNewPath } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
@@ -48,11 +51,14 @@ const TestDriveLoader: React.FunctionComponent< {
 );
 
 const TestAccountStep = () => {
-	const { currentStep, refreshOnboardingSteps } = useOnboardingContext();
+	const { currentStep, navigateToNextStep, closeModal } =
+		useOnboardingContext();
 	const [ testDriveLoaderProgress, setTestDriveLoaderProgress ] =
 		useState( 5 );
 	const [ errorMessage, setErrorMessage ] = useState< string | undefined >();
 	const [ retryCounter, setRetryCounter ] = useState( 0 );
+	const [ testAccountCreationSuccess, setTestAccountCreationSuccess ] =
+		useState( false );
 
 	// Create a reference object.
 	const loaderProgressRef = useRef( testDriveLoaderProgress );
@@ -89,7 +95,7 @@ const TestAccountStep = () => {
 						'completed'
 					) {
 						// Refresh the onboarding steps to get the latest status.
-						refreshOnboardingSteps();
+						setTestAccountCreationSuccess( true );
 					}
 				} );
 			};
@@ -107,13 +113,150 @@ const TestAccountStep = () => {
 		currentStep?.errors,
 		currentStep?.actions?.init?.href,
 		currentStep?.actions?.check?.href,
-		refreshOnboardingSteps,
 		retryCounter,
 	] );
 
+	if ( testAccountCreationSuccess ) {
+		return (
+			<>
+				<WooPaymentsStepHeader onClose={ closeModal } />
+				<div className="settings-payments-onboarding-modal__step--content">
+					<div className="woocommerce-payments-test-account-step__success_content_container">
+						<div className="woocommerce-woopayments-modal__content woocommerce-payments-test-account-step__success_content">
+							<h1 className="woocommerce-payments-test-account-step__success_content_title">
+								{ __(
+									'You’re ready to test payments!',
+									'woocommerce'
+								) }
+							</h1>
+							<div className="woocommerce-woopayments-modal__content__item">
+								<div className="woocommerce-woopayments-modal__content__item__description">
+									<p>
+										{ interpolateComponents( {
+											mixedString: __(
+												"We've created a test account for you so that you can begin {{link}}testing payments on your store{{/link}}.",
+												'woocommerce'
+											),
+											components: {
+												link: (
+													<Link
+														href="https://woocommerce.com/document/woopayments/testing-and-troubleshooting/sandbox-mode/"
+														target="_blank"
+														rel="noreferrer"
+														type="external"
+													/>
+												),
+												break: <br />,
+											},
+										} ) }
+									</p>
+								</div>
+							</div>
+							<div className="woocommerce-payments-test-account-step__success-whats-next">
+								<div className="woocommerce-woopayments-modal__content__item">
+									<h2>
+										{ __( "What's next:", 'woocommerce' ) }
+									</h2>
+								</div>
+								<div className="woocommerce-woopayments-modal__content__item-flex">
+									<img
+										src={
+											WC_ASSET_URL +
+											'images/icons/store.svg'
+										}
+										alt="store icon"
+									/>
+									<div className="woocommerce-woopayments-modal__content__item-flex__description">
+										<h3>
+											{ __(
+												'Continue your store setup',
+												'woocommerce'
+											) }
+										</h3>
+										<div>
+											{ __(
+												'Finish completing the tasks required to launch your store.',
+												'woocommerce'
+											) }
+										</div>
+									</div>
+								</div>
+								<div className="woocommerce-woopayments-modal__content__item-flex">
+									<img
+										src={
+											WC_ASSET_URL +
+											'images/icons/dollar.svg'
+										}
+										alt="dollar icon"
+									/>
+									<div className="woocommerce-woopayments-modal__content__item-flex__description">
+										<h3>
+											{ __(
+												'Activate payments',
+												'woocommerce'
+											) }
+										</h3>
+										<div>
+											<p>
+												{ interpolateComponents( {
+													mixedString: __(
+														'Provide some additional details about your business so you can being accepting real payments. {{link}}Learn more{{/link}}',
+														'woocommerce'
+													),
+													components: {
+														link: (
+															<Link
+																href="https://woocommerce.com/document/woopayments/startup-guide/#sign-up-process"
+																target="_blank"
+																rel="noreferrer"
+																type="external"
+															/>
+														),
+													},
+												} ) }
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<Button
+								variant="primary"
+								onClick={ () => {
+									// Navigate to wc-admin page
+									navigateTo( {
+										url: getNewPath( {}, '', {
+											page: 'wc-admin',
+										} ),
+									} );
+								} }
+							>
+								{ __( 'Continue store setup', 'woocommerce' ) }
+							</Button>
+							<div className="woocommerce-payments-test-account-step__success_content_or-divider">
+								<hr />
+								{ __( 'OR', 'woocommerce' ) }
+								<hr />
+							</div>
+
+							<Button
+								variant="secondary"
+								onClick={ () => {
+									// This will refresh the steps and move the modal to the next step
+									navigateToNextStep();
+								} }
+							>
+								{ __( 'Activate payments', 'woocommerce' ) }
+							</Button>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
+
 	return (
 		<div className="woocommerce-payments-test-account-step">
-			<WooPaymentsStepHeader onClose={ () => {} } />
+			<WooPaymentsStepHeader onClose={ closeModal } />
 			{ errorMessage && (
 				<Notice
 					status="warning"
@@ -133,7 +276,7 @@ const TestAccountStep = () => {
 							variant: 'secondary',
 							className:
 								'woocommerce-payments-test-account-step__error-cancel-button',
-							onClick: () => {},
+							onClick: closeModal,
 						},
 					] }
 					className="woocommerce-payments-test-account-step__error"
