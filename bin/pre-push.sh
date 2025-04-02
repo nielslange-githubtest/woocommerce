bdb4f5aee8cf3ce102f34f14400b822e6063c454
Vladimir Reznichenko <kalessil@gmail.com>
2025-04-02 10:47:10 +0200

    GIT: push hook experiments.

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -33,8 +33,9 @@ fi
 changedFiles=$(git diff $(git merge-base HEAD origin/trunk) --relative --name-only --diff-filter=d -- '*.php' '*.js' '*.jsx' '*.ts' '*.tsx')
 if [ ! -z $changedFiles ]; then
 	echo 'Generating build matrix to pick linting jobs'
-    pnpm utils ci-jobs --base-ref origin/trunk --event 'pull_request'
-    printenv
+    ciJobs=$(CI=1 pnpm utils ci-jobs --base-ref origin/trunk --event 'pull_request' &2> 1)
+    lintingJobs=$(echo $ciJobs | grep '::set-output name=lint-jobs::')
+    echo $lintingJobs
 fi
 
 echo 'Aborting push (local development purposes)'
