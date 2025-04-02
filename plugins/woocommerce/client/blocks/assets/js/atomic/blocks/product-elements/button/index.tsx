c74039c13707a0af7225b7c6a0179e49f8e8847b
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-04-02 14:15:15 +0200

    Move supports to block.json for Product Elements Button

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/button/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/button/index.tsx
@@ -12,47 +12,6 @@ import edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
-const featurePluginSupport = {
-	...metadata.supports,
-
-	color: {
-		text: true,
-		background: true,
-		link: false,
-		__experimentalSkipSerialization: true,
-	},
-	__experimentalBorder: {
-		radius: true,
-		__experimentalSkipSerialization: true,
-	},
-	...( typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
-		spacing: {
-			margin: true,
-			padding: true,
-			__experimentalSkipSerialization: true,
-		},
-		typography: {
-			fontSize: true,
-			lineHeight: true,
-			__experimentalFontWeight: true,
-			__experimentalFontFamily: true,
-			__experimentalFontStyle: true,
-			__experimentalTextTransform: true,
-			__experimentalTextDecoration: true,
-			__experimentalLetterSpacing: true,
-			__experimentalDefaultControls: {
-				fontSize: true,
-			},
-		},
-		__experimentalSelector:
-			'.wp-block-button.wc-block-components-product-button .wc-block-components-product-button__button',
-	} ),
-	...( typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
-		spacing: {
-			margin: true,
-		},
-	} ),
-};
 // @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
 registerBlockType( metadata, {
 	apiVersion: 3,
@@ -66,9 +25,6 @@ registerBlockType( metadata, {
 	},
 	attributes: {
 		...metadata.attributes,
-	},
-	supports: {
-		...featurePluginSupport,
 	},
 	edit,
 	save,
